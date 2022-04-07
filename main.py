7fd06963e2915536be7b2242b82e11cdcfcf014a
jdenver23 <denverjacob23@gmail.com>
2022-04-06 19:36:56 -0700

    Updated Speak

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-#Jacob Denver Main Branch
+#Main Branch
 import os
 import time
 from playsound import playsound
+from pathlib import Path
 import speech_recognition as sr
 from gtts import gTTS
 
-def speak(text, lang):
-    tts = gTTS(text=text, lang=lang)
-    filename = "voice.mp3"
+def speak(text, language):
+    tts = gTTS(text=text, lang=language)
+    SCRIPT_DIR = Path(__file__).parent
+    filename = SCRIPT_DIR / "voice.mp3"
     tts.save(filename)
-    playsound('./' + filename)
+    playsound(filename)
 
 def get_audio():
     r = sr.Recognizer()
     with sr.Microphone() as source:
+        speak("What would you like to translate?", "en")
         audio = r.listen(source)
         said = ""
 
@@ -23,5 +26,6 @@ def get_audio():
         except Exception as e:
             print("Exception: " + str(e))
     return said
-    
-speak("test", "en")
+
+text = get_audio() 
+speak(text, "en")
