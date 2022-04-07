399bdc7478e5134821e985e6e59a8e30d9efd8cc
lindsayly <lindsay.ly1225@gmail.com>
2022-04-06 19:06:42 -0700

    Update main.py
    Updated main

--- main.py
+++ main.py
@@ -11,4 +11,17 @@ def speak(text, lang):
     tts.save(filename)
     playsound('./' + filename)
 
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        said = ""
+
+        try:
+            said = r.recognize_google(audio)
+            print(said)
+        except Exception as e:
+            print("Exception: " + str(e))
+    return said
+    
 speak("test", "en")
